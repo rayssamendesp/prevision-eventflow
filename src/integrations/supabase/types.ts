12d0fda3213b2cbadef3398ca868fe2420d4db11
Rayssa Mendes <116162395+rayssamendesp@users.noreply.github.com>
2026-08-25 13:39:51 -0300

    Fix generated composite type reference

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -261,7 +261,7 @@ export type CompositeTypes<
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
-    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["CompositeTypes"]
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
